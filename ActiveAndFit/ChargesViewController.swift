dad7d49e86c78642871448a4dcba16368f6d96f7
Huy Hoang <hoanghuy27297@gmail.com>
2020-04-28 17:36:45 +1000

    add calculation

---
+++ ActiveAndFit/ChargesViewController.swift
@@ -0,0 +1,128 @@
+//
+//  ChargesViewController.swift
+//  ActiveAndFit
+//
+//  Created by Huy Hoang on 28/4/20.
+//  Copyright © 2020 Huy Hoang. All rights reserved.
+//
+
+import UIKit
+
+class ChargesViewController: UIViewController {
+
+    @IBOutlet weak var facilityLabel: UILabel!
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var sessionLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var discountLabel: UILabel!
+
+    var selectedFacility = ""
+
+    var selectedDay = ""
+    var selectedDayIndex = 0
+
+    var selectedStartTimeInt = 0
+    var selectedStartTime = ""
+
+    var selectedEndTimeInt = 0
+    var selectedEndTime = ""
+
+    var selectedSession = 0
+
+    var total = 0.0
+    var discount = 0.0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        facilityLabel.text = selectedFacility
+        dayLabel.text = selectedDay
+        sessionLabel.text = String(selectedSession)
+        durationLabel.text = selectedStartTime + " - " + selectedEndTime
+        calcCharges()
+    }
+
+    func calcCharges() {
+        if selectedFacility == "Pool" {
+            // if selectedDayIndex in range 0,4 -> selected day is between Monday-Friday
+            if selectedDayIndex >= 1 && selectedDayIndex <= 5 {
+                total = calcFee(offPeakFee: 20, peakFee: 25, peakHour: 1800)
+            } else {
+                total = calcFee(offPeakFee: 20, peakFee: 25, peakHour: 1200)
+            }
+        } else if selectedFacility == "Tennis court" || selectedFacility ==  "Squash court" || selectedFacility ==  "Yoga room" {
+            // if selectedDayIndex in range 0,4 -> selected day is between Monday-Friday
+            if selectedDayIndex >= 1 && selectedDayIndex <= 5 {
+                total = calcFee(offPeakFee: 30, peakFee: 35, peakHour: 1800)
+            } else {
+                total = calcFee(offPeakFee: 30, peakFee: 35, peakHour: 1200)
+            }
+        } else {
+            // if selectedDayIndex in range 0,4 -> selected day is between Monday-Friday
+            if selectedDayIndex >= 1 && selectedDayIndex <= 5 {
+                total = calcFee(offPeakFee: 40, peakFee: 45, peakHour: 1800)
+            } else {
+                total = calcFee(offPeakFee: 40, peakFee: 45, peakHour: 1200)
+            }
+        }
+
+        totalLabel.text = "$" + String(format:"%.2f", total)
+        if total > 100 {
+            discount = (total * 10) / 100
+            total -= discount
+            discountLabel.text = "discount 10%: $" + String(format:"%.2f", discount) + " = $" + String(format:"%.2f", total)
+        } else {
+            discountLabel.text = ""
+        }
+    }
+
+    func calcFee(offPeakFee: Int, peakFee: Int, peakHour: Int) -> Double {
+        // compare the start time and end time with range of peak hours
+        if selectedEndTimeInt < peakHour {
+            // selection is in off-peak hours -> end time must be before 18:00
+            return Double(offPeakFee * selectedSession)
+        } else if selectedStartTimeInt > peakHour {
+            // selection is in peak hours -> start time must be after 18:00
+            return Double(peakFee * selectedSession)
+        } else {
+            // selection is in between off-peak hours and peak hours -> start time must be before 18:00 and end time is after 18:00
+            let firstHalf = peakHour - selectedStartTimeInt
+            let secondHalf = selectedEndTimeInt - peakHour
+
+            var fee = 0.0
+
+            if firstHalf % 100 == 0 {
+                // the number of sessions from start time to peak hour is integer
+                fee += Double((firstHalf / 100) * offPeakFee)
+            } else {
+                // the fee includes half an hour of the normal fee
+                print(round(Double(firstHalf / 100)) + 0.5)
+                fee += (round(Double(firstHalf / 100)) + 0.5) * Double(offPeakFee)
+                print("Fee " + String(fee))
+            }
+
+            if secondHalf % 100 == 0 {
+                // the number of sessions from start time to peak hour is integer
+                fee += Double((secondHalf / 100) * peakFee)
+            } else {
+                // the fee includes half an hour of the normal fee
+                fee += (round(Double(secondHalf / 100)) + 0.5) * Double(peakFee)
+            }
+
+            return fee
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- ScheduleViewController.swift
+++ ScheduleViewController.swift
@@ -23,18 +23,26 @@ class ScheduleViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
     let sessionPickerView = UIPickerView()
     
     var selectedFacilityName = ""
+    var selectedFacilityIndex = 0
 
-    var selectedDay: String?
-    var dayList = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    var selectedDay = ""
+    var dayList = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    var selectedDayIndex = 0
 
-    var selectedStartTime: String?
+    var selectedStartTime = ""
+    var selectedStartTimeInt = 0
     var startTimeList = ["6:00", "6:30", "7:00", "7:30", "8:00", "8:30", "9:00", "9:30", "10:00", "10:30",
                         "11:00", "11:30", "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30",
                         "16:00", "16:30", "17:00", "17:30", "18:00", "18:30", "19:00", "19:30", "20:00", "20:30",
                         "21:00", "21:30", "22:00"]
+    var startTimeIntList = [600, 630, 700, 730, 800, 830, 900, 930, 1000, 1030,
+                            1100, 1130, 1200, 1230, 1300, 1330, 1400, 1430, 1500, 1530,
+                            1600, 1630, 1700, 1730, 1800, 1830, 1900, 1930, 2000, 2030,
+                            2100, 2130, 2200]
+    var startTimeIndex = 0
 
-    var selectedSession: Int?
-    var sessionList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
+    var selectedSession = 0
+    var sessionList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -72,10 +80,15 @@ class ScheduleViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         if pickerView == dayPickerView {
             selectedDay = dayList[row]
+            selectedDayIndex = row
             dayTextField.text = selectedDay
         } else if pickerView == startTimePickerView {
             selectedStartTime = startTimeList[row]
+            selectedStartTimeInt = startTimeIntList[row]
+            startTimeIndex = row
             startTimeTextField.text = selectedStartTime
+
+            changeSessionList(row: row)
         } else {
             selectedSession = sessionList[row]
             sessionTextField.text = String(sessionList[row])
@@ -90,6 +103,38 @@ class ScheduleViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
         dayTextField.inputView = dayPickerView
         startTimeTextField.inputView = startTimePickerView
         sessionTextField.inputView = sessionPickerView
+
+        // get the initial day of week in number
+        let calendar = Calendar.autoupdatingCurrent
+        // get index of the current day and assign to variables
+        selectedDayIndex = calendar.component(.weekday, from: Date()) - 1
+        selectedDay = dayList[selectedDayIndex]
+        dayPickerView.selectRow(selectedDayIndex, inComponent: 0, animated: false)
+        dayTextField.text = selectedDay
+
+        // get initial start time
+        let hour = calendar.component(.hour, from: Date())
+        let minute = calendar.component(.minute, from: Date())
+        // get the index of current hour
+        startTimeIndex = startTimeIntList.firstIndex(of: hour * 100)!
+        if minute - 30 < 0 {
+            // the current time is before half an hour e.g 16:15
+            // then the initial start time is 16:30
+            startTimeIndex += 1
+        } else {
+            // otherwise the start time is 17:00
+            startTimeIndex += 2
+        }
+        selectedStartTime = startTimeList[startTimeIndex]
+        selectedStartTimeInt = startTimeIntList[startTimeIndex]
+        startTimePickerView.selectRow(startTimeIndex, inComponent: 0, animated: false)
+        startTimeTextField.text = selectedStartTime
+        changeSessionList(row: startTimeIndex)
+
+        // assign initial value for the number of sessions as the first index in the list
+        selectedSession = sessionList[0]
+        sessionPickerView.selectRow(0, inComponent: 0, animated: false)
+        sessionTextField.text = String(selectedSession)
     }
 
     func dismissPickerView() {
@@ -108,16 +153,47 @@ class ScheduleViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
     @objc func action() {
         view.endEditing(true)
     }
-    
 
-    /*
+    func changeSessionList(row: Int) {
+        // after chosing the start time, the range of session available must be changed
+        var startTimeInt = selectedStartTimeInt
+        if selectedStartTimeInt % 100 != 0 {
+            // if the start time is not even e.g 16:30, the number of available session must be reduce 1
+            startTimeInt = startTimeIntList[row + 1]
+        }
+        let maxRangeSession = (2200 - startTimeInt) / 100
+        var tempSessionList = Array<Int>()
+        for i in 1...maxRangeSession {
+            tempSessionList.append(i)
+        }
+        sessionList = tempSessionList
+    }
+
+    @IBAction func submitButton(_ sender: Any) {
+        
+    }
+
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        if segue.identifier == "ShowCharges" {
+            let chargesViewController = segue.destination as! ChargesViewController
+
+            chargesViewController.selectedFacility = selectedFacilityName
+
+            chargesViewController.selectedDay = selectedDay
+            chargesViewController.selectedDayIndex = selectedDayIndex
+
+            chargesViewController.selectedSession = selectedSession
+            chargesViewController.selectedStartTime = selectedStartTime
+            chargesViewController.selectedStartTimeInt = selectedStartTimeInt
+            chargesViewController.selectedEndTime = startTimeList[startTimeIndex + selectedSession * 2]
+            chargesViewController.selectedEndTimeInt = startTimeIntList[startTimeIndex + selectedSession * 2]
+        }
     }
-    */
+
 
 }
