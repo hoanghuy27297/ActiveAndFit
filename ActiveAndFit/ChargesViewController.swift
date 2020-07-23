88d0fc5d2f3b9dcffffe7e1dd30bdd60709d0bda
Huy Hoang <hoanghuy27297@gmail.com>
2020-07-23 19:51:37 +1000

    add specification

--- ActiveAndFit/ChargesViewController.swift
+++ ActiveAndFit/ChargesViewController.swift
@@ -50,7 +50,6 @@ class ChargesViewController: UIViewController {
             if selectedDayIndex >= 1 && selectedDayIndex <= 5 {
                 total = calcFee(offPeakFee: 20, peakFee: 25, peakHour: 1800)
             } else {
-                print("aaa")
                 total = calcFee(offPeakFee: 20, peakFee: 25, peakHour: 1200)
             }
         } else if selectedFacility == "Tennis court" || selectedFacility ==  "Squash court" || selectedFacility ==  "Yoga room" {
@@ -70,7 +69,7 @@ class ChargesViewController: UIViewController {
         }
 
         totalLabel.text = "$" + String(format:"%.2f", total)
-        if total > 100 {
+        if total >= 100 {
             discount = (total * 10) / 100
             total -= discount
             discountLabel.text = "discount 10%: $" + String(format:"%.2f", discount) + " = $" + String(format:"%.2f", total)
@@ -81,35 +80,55 @@ class ChargesViewController: UIViewController {
 
     func calcFee(offPeakFee: Int, peakFee: Int, peakHour: Int) -> Double {
         // compare the start time and end time with range of peak hours
-        if (selectedEndTimeInt < peakHour && peakHour == 1800) || (selectedEndTimeInt > peakHour && peakHour == 1200) {
+        if (selectedEndTimeInt < peakHour && peakHour == 1800) || (selectedStartTimeInt > peakHour && peakHour == 1200) {
             // selection is in off-peak hours -> end time must be before 18:00
             return Double(offPeakFee * selectedSession)
-        } else if (selectedEndTimeInt > peakHour && peakHour == 1800) || (selectedEndTimeInt < peakHour && peakHour == 1200) {
+        } else if (selectedStartTimeInt > peakHour && peakHour == 1800) || (selectedEndTimeInt < peakHour && peakHour == 1200) {
             // selection is in peak hours -> start time must be after 18:00
             return Double(peakFee * selectedSession)
         } else {
             // selection is in between off-peak hours and peak hours -> start time must be before 18:00 and end time is after 18:00
             let firstHalf = peakHour - selectedStartTimeInt
             let secondHalf = selectedEndTimeInt - peakHour
+            
+            print(firstHalf)
+            print(secondHalf)
 
             var fee = 0.0
 
             if firstHalf % 100 == 0 {
                 // the number of sessions from start time to peak hour is integer
-                fee += Double((firstHalf / 100) * offPeakFee)
+                // the different between weekdays and weekends
+                // hours from the open hour to peak hour on weekdays are off-peak hours and then is peak hours
+                // meanwhile on weekends, these hours are peak hours and then is off-peak hours
+                if (peakHour == 1800) {
+                    fee += Double((firstHalf / 100) * offPeakFee)
+                } else {
+                    fee += Double((firstHalf / 100) * peakFee)
+                }
             } else {
                 // the fee includes half an hour of the normal fee
-                print(round(Double(firstHalf / 100)) + 0.5)
-                fee += (round(Double(firstHalf / 100)) + 0.5) * Double(offPeakFee)
-                print("Fee " + String(fee))
+                if (peakHour == 1800) {
+                    fee += (round(Double(firstHalf / 100)) + 0.5) * Double(offPeakFee)
+                } else {
+                    fee += (round(Double(firstHalf / 100)) + 0.5) * Double(peakFee)
+                }
             }
 
             if secondHalf % 100 == 0 {
                 // the number of sessions from start time to peak hour is integer
-                fee += Double((secondHalf / 100) * peakFee)
+                if (peakHour == 1800) {
+                    fee += Double((secondHalf / 100) * peakFee)
+                } else {
+                    fee += Double((secondHalf / 100) * offPeakFee)
+                }
             } else {
                 // the fee includes half an hour of the normal fee
-                fee += (round(Double(secondHalf / 100)) + 0.5) * Double(peakFee)
+                if (peakHour == 1800) {
+                    fee += (round(Double(secondHalf / 100)) + 0.5) * Double(peakFee)
+                } else {
+                    fee += (round(Double(secondHalf / 100)) + 0.5) * Double(offPeakFee)
+                }
             }
 
             return fee
